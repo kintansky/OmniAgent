534405b310af22741856439742d9715ac2be3b72
jx_tan@outlook.com <!1qaz2WSX!>
2019-03-10 00:45:56 +0800

    add export function to moudlelist views

--- inspection/urls.py
+++ inspection/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('moudle/', views.moudle_list, name='moudle_list'),
     path('moudle/search/', views.search_moudle, name='search_moudle'),
+    path('moudle/download/', views.export_moudle, name='export_moudle'),
 ]
--- inspection/views.py
+++ inspection/views.py
@@ -3,7 +3,8 @@ from .models import OpticalMoudleDiff
 import datetime
 from django.utils import timezone
 from .forms import MoudleSearchForm
-from funcpack.funcs import pages
+from funcpack.funcs import pages, exportXls
+from django.http import FileResponse
 
 # Create your views here.
 def moudle_list(request):
@@ -56,3 +57,29 @@ def search_moudle(request):
     context['moudle_search_form'] = moudle_search_form
     
     return render(request, 'moudle_list.html', context)
+
+def export_moudle(request):
+    device_name = request.GET.get('device_name', '')
+    status = request.GET.get('status', '')
+    time_begin = request.GET.get('time_begin', '')
+    time_end = request.GET.get('time_end', '')
+    if time_begin == '' or time_end == '':
+        today_time = timezone.datetime.now()
+        time_end = timezone.datetime(year=today_time.year, month=today_time.month, day=today_time.day, hour=23, minute=59, second=59)
+        time_begin = time_end + timezone.timedelta(days=-1)  # 默认下载前一天的数据
+    else:
+        time_begin = datetime.datetime.strptime(time_begin, '%Y-%m-%d+%H:%M:%S')
+        time_end = datetime.datetime.strptime(time_end, '%Y-%m-%d+%H:%M:%S')
+    time_range = (time_begin, time_end)
+    if device_name == status == '':
+        moudle_all_list = OpticalMoudleDiff.objects.filter(record_time__range=time_range)
+    else:
+        if device_name != '':
+            moudle_all_list = OpticalMoudleDiff.objects.filter(device_name__icontains=device_name, status__contains=status, record_time__range=time_range)
+        elif status != '':
+            moudle_all_list = OpticalMoudleDiff.objects.filter(status=status, record_time__range=time_range)
+        else:
+            moudle_all_list = OpticalMoudleDiff.objects.filter(record_time__range=time_range)
+    output = exportXls(OpticalMoudleDiff._meta.fields, moudle_all_list)
+    response = FileResponse(output, as_attachment=True, filename="moudle_result.xls") # 使用Fileresponse替代以上两行
+    return response
