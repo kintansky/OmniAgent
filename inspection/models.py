2c6bbb547f0ef9ff4d05ef82ece1d333a27588dc
jx_tan@outlook.com <!1qaz2WSX!>
2019-03-27 09:07:25 +0800

    add PortErrorDiff model in app inspection

--- inspection/models.py
+++ inspection/models.py
@@ -16,3 +16,21 @@ class OpticalMoudleDiff(models.Model):
 
     class Meta:
         ordering = ['-record_time']
+
+class PortErrorDiff(models.Model):
+    device_name = models.CharField(max_length=255)
+    port = models.CharField(max_length=30)
+    nowCRC = models.IntegerField()
+    nowIpv4HeaderError = models.IntegerField()
+    everCRC = models.IntegerField()
+    everIpv4HeaderError = models.IntegerField()
+    STATE_CHOICES = (
+        ('increase', 'INCREASING'),
+        ('fair', 'fair'),
+    )
+    stateCRC = models.CharField(max_length=20, choices=STATE_CHOICES)
+    stateIpv4HeadError = models.CharField(max_length=20, choices=STATE_CHOICES)
+    recordTime = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-record_time']
