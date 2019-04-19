732c0651cf52d6be0e9bb8fc628f099e5514a62c
jx_tan@outlook.com <!1qaz2WSX!>
2019-04-19 13:55:02 +0800

    mod publicip_segment admin model

--- networkresource/admin.py
+++ networkresource/admin.py
@@ -4,4 +4,4 @@ from .models import PublicIpSegment
 # Register your models here.
 @admin.register(PublicIpSegment)
 class PublicIpSegmentAdmin(admin.ModelAdmin):
-    list_display = ('id', 'ip_segment', 'mask')
+    list_display = ('id', 'ip_segment', 'mask', 'segment_type')
