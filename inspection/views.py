809c241db5be9a7a22caf8ff971e86cadc392b28
jx_tan@outlook.com <!1qaz2WSX!>
2019-05-24 22:47:13 +0800

    add sort func to port error views

--- inspection/views.py
+++ inspection/views.py
@@ -83,13 +83,8 @@ def export_moudle(request):
     response = FileResponse(open(output, 'rb'), as_attachment=True, filename="moudle_result.xls") # 使用Fileresponse替代以上两行
     return response
 
-# port error 采用rawquery做连接查询，取出其他关联信息 
-PORTERROR_QUERY = 'SELECT np.*, \
-                    ni.port_description, ni.port_status \
-                    FROM networkresource_porterrordiff as np LEFT JOIN networkresource_ipmanresource AS ni \
-                    ON np.device_name = ni.device_name AND np.port = ni.port'
-# PORTERROR_QUERY2 用于请求CRC+光功率的信息
-PORTERROR_QUERY2 = "\
+# port error 采用rawquery做连接查询，取出其他关联信息 用于请求CRC+光功率的信息
+__PORTERROR_QUERY = "\
     SELECT error_info.*, npp.* FROM (\
         SELECT np.*, ni.port_description, ni.port_status \
             FROM networkresource_porterrordiff as np \
@@ -98,21 +93,29 @@ PORTERROR_QUERY2 = "\
             WHERE np.record_time between %s AND %s\
         ) AS error_info \
     LEFT JOIN (\
-        SELECT device_name, `port`, tx_now_power, tx_state, rx_now_power, rx_state \
+        SELECT device_name, `port`, tx_now_power, tx_high_warm, tx_low_warm, tx_state, rx_now_power, rx_high_warm, rx_low_warm, rx_state, record_time \
             FROM cmdb.networkresource_portperf \
             WHERE record_time BETWEEN %s AND %s\
         ) AS npp \
     ON error_info.device_name = npp.device_name AND error_info.port = npp.port \
-    ORDER BY -error_info.stateCRC \
-    "
-    
+    AND DATE_FORMAT(error_info.record_time, '%Y-%m-%d') = DATE_FORMAT(npp.record_time, '%Y-%m-%d') \
+"
+
+def __queryline(order_field):
+    if order_field == 'crc':
+        porterror_query = __PORTERROR_QUERY+'ORDER BY -error_info.stateCRC'
+    elif order_field == 'head':
+        porterror_query = __PORTERROR_QUERY+'ORDER BY -error_info.stateIpv4HeadError'
+    return porterror_query
+
 def port_error_list(request):
-    time_begin, time_end = getDateRange(-2)
+    order_field = request.GET.get('order_field', 'crc')
+    time_begin, time_end = getDateRange(-60)
     time_range = (time_begin, time_end)
+    porterror_query = __queryline(order_field)
     porterror_all_list = PortErrorDiff.objects.raw(
-        PORTERROR_QUERY2, (time_begin, time_end, time_begin, time_end)
+        porterror_query, (time_begin, time_end, time_begin, time_end)
     )
-    # porterror_all_list = PortErrorDiff.objects.raw(PORTERROR_QUERY)
     page_of_objects, page_range = pages(request, porterror_all_list)
 
     context = {}
@@ -121,21 +124,20 @@ def port_error_list(request):
     context['page_range'] = page_range
     context['time_begin'] = timezone.datetime.strftime(time_range[0], '%Y-%m-%d+%H:%M:%S')
     context['time_end'] = timezone.datetime.strftime(time_range[1], '%Y-%m-%d+%H:%M:%S')
+    context['order_field'] = order_field
     context['porterror_search_form'] = PortErrorSearchForm()
     return render(request, 'port_error_list.html', context)
 
 def search_port_error(request):
     context = {}
+    order_field = request.GET.get('order_field', 'crc')
     porterror_search_form = PortErrorSearchForm(request.GET)
     if porterror_search_form.is_valid():
         time_begin = porterror_search_form.cleaned_data['time_begin']
         time_end = porterror_search_form.cleaned_data['time_end']
-        # time_range = (time_begin, time_end)
-        # porterror_all_list = PortErrorDiff.objects.raw(
-        #     PORTERROR_QUERY + ' WHERE np.record_time between %s and %s', time_range
-        # )
+        porterror_query = __queryline(order_field)
         porterror_all_list = PortErrorDiff.objects.raw(
-            PORTERROR_QUERY2, (time_begin, time_end, time_begin, time_end)
+            porterror_query, (time_begin, time_end, time_begin, time_end)
         )
     else:
         context['porterror_search_form'] = porterror_search_form
@@ -148,6 +150,7 @@ def search_port_error(request):
     context['page_range'] = page_range
     context['time_begin'] = timezone.datetime.strftime(time_begin, '%Y-%m-%d+%H:%M:%S')
     context['time_end'] = timezone.datetime.strftime(time_end, '%Y-%m-%d+%H:%M:%S')
+    context['order_field'] = order_field
     context['porterror_search_form'] = porterror_search_form
     return render(request, 'port_error_list.html', context)
 
@@ -161,12 +164,9 @@ def export_porterror(request):
     else:
         time_begin = timezone.datetime.strptime(time_begin, '%Y-%m-%d %H:%M:%S')
         time_end = timezone.datetime.strptime(time_end, '%Y-%m-%d %H:%M:%S')
-    time_range = (time_begin, time_end)
-    # porterror_all_list = PortErrorDiff.objects.raw(
-    #     PORTERROR_QUERY + ' WHERE np.record_time between %s and %s', time_range
-    # )   # 返回的事RawQuerySet对象，原有的导出函数不适用
+    porterror_query = __queryline('crc')
     porterror_all_list = PortErrorDiff.objects.raw(
-        PORTERROR_QUERY2, (time_begin, time_end, time_begin, time_end)
+        porterror_query, (time_begin, time_end, time_begin, time_end)
     )
     output = rawQueryExportXls(porterror_all_list.columns, porterror_all_list, 'record_time')
     response = FileResponse(open(output, 'rb'), as_attachment=True, filename="porterror_result.xls")
