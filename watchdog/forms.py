96696d6dcd630426f1a63b6a736883944708365e
jx_tan@outlook.com <!1qaz2WSX!>
2019-05-25 12:02:41 +0800

    format files

--- watchdog/forms.py
+++ watchdog/forms.py
@@ -1,14 +1,22 @@
 from django import forms
 from .models import DeviceManufactor, Device
 
+
 class AddDeviceForm(forms.Form):
-    device_name = forms.CharField(label='Device Name', widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'GDFOS-IPMAN-BNG01-DS-HW'}))
-    device_ip = forms.GenericIPAddressField(label='IP Address', protocol='both', widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': '1.1.1.1'}))
-    device_manufactor = forms.CharField(label='Manufactor', widget=forms.TextInput(attrs={'class': 'form-control','placeholder': 'huawei'}))
-    device_network = forms.CharField(label='Network', widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'IPMAN'}))
-    login_user = forms.CharField(label='User', widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'admin'}))
-    login_port = forms.IntegerField(label='Port', widget=forms.NumberInput(attrs={'class': 'form-control', 'placeholder': 22}))
-    login_password = forms.CharField(label='Password', widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'password'}))
+    device_name = forms.CharField(label='Device Name', widget=forms.TextInput(
+        attrs={'class': 'form-control', 'placeholder': 'GDFOS-IPMAN-BNG01-DS-HW'}))
+    device_ip = forms.GenericIPAddressField(label='IP Address', protocol='both', widget=forms.TextInput(
+        attrs={'class': 'form-control', 'placeholder': '1.1.1.1'}))
+    device_manufactor = forms.CharField(label='Manufactor', widget=forms.TextInput(
+        attrs={'class': 'form-control', 'placeholder': 'huawei'}))
+    device_network = forms.CharField(label='Network', widget=forms.TextInput(
+        attrs={'class': 'form-control', 'placeholder': 'IPMAN'}))
+    login_user = forms.CharField(label='User', widget=forms.TextInput(
+        attrs={'class': 'form-control', 'placeholder': 'admin'}))
+    login_port = forms.IntegerField(label='Port', widget=forms.NumberInput(
+        attrs={'class': 'form-control', 'placeholder': 22}))
+    login_password = forms.CharField(label='Password', widget=forms.TextInput(
+        attrs={'class': 'form-control', 'placeholder': 'password'}))
 
     def clean(self):    # is_valid自动调用
         dv = self.cleaned_data['device_name']
@@ -23,5 +31,5 @@ class AddDeviceForm(forms.Form):
             pass
         else:
             self.add_error('device_manufactor', '暂不支持的厂家')
-        
+
         return self.cleaned_data
