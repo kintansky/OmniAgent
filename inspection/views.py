3e5609a4d0d8ff601fa358c776a69b5642288ccc
jx_tan@outlook.com <!1qaz2WSX!>
2019-07-02 08:30:55 +0800

    fix a bug in my port error task

--- inspection/views.py
+++ inspection/views.py
@@ -453,18 +453,19 @@ def ajax_port_operate(request, operation_type):
 
 
 __QUERY_MY_FIX_TASKS = "\
-    SELECT fix_info.*, npp.tx_now_power, npp.tx_high_warm, npp.tx_low_warm, npp.tx_state, npp.rx_now_power, npp.rx_high_warm, npp.rx_low_warm, npp.rx_state, npp.utility_in, npp.utility_out FROM (\
-        SELECT * FROM(\
-            SELECT ped.*, pef.worker FROM \
-                omni_agent.inspection_porterrorfixrecord as pef \
-            left JOIN omni_agent.inspection_porterrordiff AS ped \
-            ON pef.device_name = ped.device_name AND pef.port = ped.port HAVING pef.worker = %s \
-            ORDER BY ped.record_time DESC \
-        ) AS recent_tb GROUP BY recent_tb.device_name, recent_tb.port \
-    ) AS fix_info \
-    LEFT JOIN omni_agent.inspection_portperf AS npp \
-    ON fix_info.device_name = npp.device_name AND fix_info.port = npp.port AND DATE_FORMAT(fix_info.record_time, '%Y-%m-%d') = DATE_FORMAT(npp.record_time, '%Y-%m-%d') \
-    ORDER BY fix_info.record_time DESC, fix_info.fix_status \
+SELECT fix_info.*, npp.tx_now_power, npp.tx_high_warm, npp.tx_low_warm, npp.tx_state, npp.rx_now_power, npp.rx_high_warm, npp.rx_low_warm, npp.rx_state, npp.utility_in, npp.utility_out FROM (\
+	SELECT * FROM(\
+	 	SELECT ped.*, pef.worker FROM \
+		omni_agent.inspection_porterrorfixrecord as pef \
+		left JOIN omni_agent.inspection_porterrordiff AS ped \
+		ON pef.device_name = ped.device_name AND pef.port = ped.port \
+        HAVING pef.worker = %s AND (ped.stateCRC >= 60 OR ped.stateIpv4HeadError >= 1000) \
+		ORDER BY ped.record_time DESC \
+	) AS recent_tb GROUP BY recent_tb.device_name, recent_tb.port \
+) AS fix_info \
+LEFT JOIN omni_agent.inspection_portperf AS npp \
+ON fix_info.device_name = npp.device_name AND fix_info.port = npp.port AND DATE_FORMAT(fix_info.record_time, '%Y-%m-%d') = DATE_FORMAT(npp.record_time, '%Y-%m-%d') \
+ORDER BY fix_info.record_time DESC, fix_info.fix_status \
 "
 
 
