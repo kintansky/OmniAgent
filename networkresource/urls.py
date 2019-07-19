3f1563b4bd8b4de569bf16b2192e85230f557866
jx_tan@outlook.com <!1qaz2WSX!>
2019-07-19 14:10:57 +0800

    delete old ip allocation func

--- networkresource/urls.py
+++ networkresource/urls.py
@@ -5,7 +5,6 @@ urlpatterns = [
     path('ip/', views.ip_list, name='ip_record'),
     path('ip/search/', views.search_ip, name='search_ip'),
     path('ip/download/', views.export_ip, name='export_ip'),
-    path('ip/<str:ip_type>/allocate/', views.allocate_ip, name='allocate_ip'),
     # new ip allocate
     path('ip/allocate/', views.new_allocate_ip, name='new_ip_allocate'),
     path('ip/allocate/generate_ip/', views.ajax_generate_ip_list, name='generate_ip'),
@@ -17,9 +16,5 @@ urlpatterns = [
     path('ip/locate_allocated_ip/', views.ajax_locate_allocated_ip, name='locate_allocated_ip'),
     path('ip/mod_allocated_ip/', views.ajax_mod_allocated_ip, name='mod_allocated_ip'),
 
-    path('allocated_ip/<str:ip_type>/', views.allocate_ip_list, name='allocated_ip_list'),
-    path('allocated_ip/<str:ip_type>/search/', views.search_allocated_ip, name='search_allocated_ip'),
-    path('ajax_locate_ip/<str:ip_type>/', views.ajax_locate_ip, name='ajax_locate_ip'),
-    path('ip_alocation_mod/<str:ip_type>/', views.ip_allocation_mod, name='ip_allocation_mod'),
     path('ajax_search_slot_ports/', views.ajax_search_slot_ports, name='ajax_search_slot_ports'),
 ]
